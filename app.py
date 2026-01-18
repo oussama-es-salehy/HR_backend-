155c7188b012cdc73c6c5ad3f496285d946e18ff
oussama-es-salehy <ousamasalhi85@gmail.com>
2026-01-18 02:57:16 +0100

    v4

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def get_candidates(job_id):
     return jsonify(candidates)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
